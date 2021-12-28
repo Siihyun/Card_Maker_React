df0af96415fdb987e2c0e942fcc0566870ff5294
Sihyun <sihyun930222@gmail.com>
2021-12-29 01:01:27 +0900

    <refactor> adjust routing setting

--- card-maker/src/App.tsx
+++ card-maker/src/App.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './page/login/Login';
 import Main from './page/main/Main';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/main' element={<Main />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/main' element={<Main />} />
+    </Routes>
   );
 }
 

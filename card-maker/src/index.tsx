66ab0069117c2cbd5e805266d4c9a9bad8582c46
Sihyun <sihyun930222@gmail.com>
2021-12-29 01:02:17 +0900

    <feat> apply theme provider

--- card-maker/src/index.tsx
+++ card-maker/src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@emotion/react';
+import theme from './styles/theme';
+import GlobalStyle from './styles/global';
 
 ReactDOM.render(
   <React.StrictMode>
--- card-maker/src/page/main/Main.tsx
+++ card-maker/src/page/main/Main.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
 const Main = () => {
-  return <div>MainPage</div>;
+  return <SomeText>MainPage</SomeText>;
 };
+
+const SomeText = styled.div`
+  color: ${({ theme }) => theme.colors.makerPink};
+  border: 3px solid ${(props) => props.theme.colors.makerBlack};
+`;
 
 export default Main;

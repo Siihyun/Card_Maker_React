9f62e227f80e2ab16fde3569b7bf6a8933df50e1
Sihyun <sihyun930222@gmail.com>
2021-12-28 22:26:50 +0900

    <feat> add component routing logic

--- card-maker/src/App.tsx
+++ card-maker/src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link } from 'react-router-dom';
 import './reset.css';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Router Test</h1>
+      <nav
+        style={{
+          borderBottom: 'solid 1px',
+          paddingBottom: '1rem',
+        }}
+      >
+        <Link to='/main'>Go to main page</Link> |{' '}
+        <Link to='/login'>Go to Login page</Link>
+      </nav>
     </div>
   );
 }
--- card-maker/src/index.tsx
+++ card-maker/src/index.tsx
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './page/login/Login';
+import Main from './page/main/Main';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='login' element={<Login />} />
+        <Route path='main' element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+    ,
   </React.StrictMode>,
   document.getElementById('root')
 );

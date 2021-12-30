465c67c80fb7fdcf0c93fc83102a82d95a049a14
Sihyun <sihyun930222@gmail.com>
2021-12-31 00:57:18 +0900

    <feat> add Header component

---
+++ card-maker/src/component/header/Header.tsx
@@ -0,0 +1,31 @@
+import styled from '@emotion/styled';
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <HeaderImg src='/images/logo.png' />
+      <HeaderText>Business Card Maker</HeaderText>
+    </HeaderWrapper>
+  );
+};
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.makerGreen};
+`;
+
+const HeaderText = styled.h1`
+  color: ${({ theme }) => theme.colors.makerWhite};
+  font-size: 2rem;
+  font-weight: bold;
+`;
+
+const HeaderImg = styled.img`
+  width: 5rem;
+  height: auto;
+`;
+
+export default Header;

098a079e2bf2a6d1884ffe13ffb21d7f6681288c
Sihyun <sihyun930222@gmail.com>
2022-01-02 01:34:06 +0900

    <feat> add component to Login Page

--- card-maker/src/page/login/Login.tsx
+++ card-maker/src/page/login/Login.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import styled from '@emotion/styled';
+import Footer from '../../component/footer/Footer';
 import Header from '../../component/header/Header';
+import LoginContent from '../../component/loginContent/LoginContent';
 
 const Login = () => {
-  return <Header />;
+  return (
+    <LoginWrapper>
+      <Header />
+      <LoginContent />
+      <Footer />
+    </LoginWrapper>
+  );
 };
+
+const LoginWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 500px;
+  height: 400px;
+  background-color: ${({ theme }) => theme.colors.makerWhite};
+`;
 
 export default Login;

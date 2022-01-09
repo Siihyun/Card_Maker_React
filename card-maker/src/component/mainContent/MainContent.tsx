7019f0157cbc8f0ec1795e9b35dc8a32f54e5e16
Sihyun <sihyun930222@gmail.com>
2022-01-09 23:33:54 +0900

    <feat> apply CardMaker and CardPreview Component to main

--- card-maker/src/component/mainContent/MainContent.tsx
+++ card-maker/src/component/mainContent/MainContent.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import CardPreview from '../cardPreview/CardPreview';
+import CardMaker from '../cardMaker/CardMaker';
 
 const MainContent = () => {
-  return <MainWrapper>a</MainWrapper>;
+  return (
+    <MainWrapper>
+      <CardMaker />
+      <Seperator />
+      <CardPreview />
+    </MainWrapper>
+  );
 };
 
 const MainWrapper = styled.div`
+  display: flex;
   height: 100%;
   background-color: ${({ theme }) => theme.colors.makerWhite};
+`;
+
+const Seperator = styled.div`
+  width: 1px;
+  background-color: ${({ theme }) => theme.colors.makerLightGrey};
 `;
 
 export default MainContent;

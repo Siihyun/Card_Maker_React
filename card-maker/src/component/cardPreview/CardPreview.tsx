49cb096be1bb21d44f7176f023f5cac71d96ac7c
Sihyun <sihyun930222@gmail.com>
2022-01-30 01:05:17 +0900

    <feat> add CardPreview Component

---
+++ card-maker/src/component/cardPreview/CardPreview.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Card } from '../mainContent/MainContent';
+
+interface Props {
+  cards: Card[];
+  setCards: React.Dispatch<React.SetStateAction<Card[]>>;
+}
+
+const CardPreview = ({ cards, setCards }: Props) => {
+  return (
+    <CardPreviewWrapper>
+      <Title>Card Preview</Title>
+    </CardPreviewWrapper>
+  );
+};
+
+const CardPreviewWrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  flex: 1;
+  padding: 2rem 1rem;
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.makerGreen};
+`;
+
+export default CardPreview;

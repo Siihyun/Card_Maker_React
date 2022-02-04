dd0e8fd04c9322624475cdc9b7845f050d56f851
Sihyun <sihyun930222@gmail.com>
2022-02-05 01:00:05 +0900

    <style> change style

--- card-maker/src/page/login/Login.tsx
+++ card-maker/src/page/login/Login.tsx
@@ -2,20 +2,27 @@ import styled from '@emotion/styled';
 import Footer from '@/component/footer/Footer';
 import Header from '@/component/header/Header';
 import LoginContent from '@/component/loginContent/LoginContent';
-import googleLogin from '@/service/googleLogin';
-import githubLogin from '@/service/githubLogin';
+import socialLogin from '@/service/authProvider';
 
 const Login = () => {
   return (
-    <LoginWrapper>
-      <Header />
-      <LoginContent googleLogin={googleLogin} githubLogin={githubLogin} />
-      <Footer />
-    </LoginWrapper>
+    <AppWrapper>
+      <LoginWrapper>
+        <Header />
+        <LoginContent socialLogin={socialLogin} />
+        <Footer />
+      </LoginWrapper>
+    </AppWrapper>
   );
 };
 
-const LoginWrapper = styled.section`
+const AppWrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+const LoginWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 500px;
